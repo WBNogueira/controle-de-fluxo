6432271f44c010e8ea9e248d07e83ef423ce083b
Wagner Nogueira <wbnogueira3@gmail.com>
2023-06-14 17:21:32 -0300

    Complemento da classe Contador e cirado a exceções

--- controle_fluxo_contador/src/Contador.java
+++ controle_fluxo_contador/src/Contador.java
@@ -2,26 +2,34 @@ import java.util.Scanner;
 
 public class Contador  {
     public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o primeiro parâmetro: ");
         int parametroUm = sc.nextInt();
+        System.out.print("Digite o segundo parâmetro: ");
         int parametroDois = sc.nextInt();
 
         try {
-            // chamando o método contando a lógicade contagem
+            // chamando o método contando a lógica de contagem
             contar(parametroUm, parametroDois);
 
-        }catch (? e) {
+        } catch (ParametrosInvalidosException e) {
             // imprimir a mensagem: O segundo parametro deve ser maior que o primeiro
+            System.out.println("O segundo parametro deve ser maior que o primeiro");
         }
-
+        sc.close();
     }
 
-    static void contar(int paramentroUmn, int parametroDois) throws ParametrosInvalidosException {
+    static void contar(int paramentroUm, int parametroDois) throws ParametrosInvalidosException {
         // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (paramentroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+        int contagem = parametroDois - paramentroUm;
 
-        int contagem = parametroDois - paramentroUmn
         // realizar o for para imprimir os números com base na variável contagem
+        for (int i = 0; i < contagem; i++) {
+            System.out.println("Imprimindo o número " + (i + 1));
+        } 
     }
 }
